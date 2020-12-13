f864d5206314b8ea9a8069430e4e31e379d9d451
Neel9784 <neel9784@gmail.com>
2020-12-13 09:02:01 +0530

    Updates

---
+++ precache-manifest.0d8111a765df71144790b9addff74731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f614c591a6d208f405a635772e94eae1",
+    "url": "/TP-PVT-LTD/index.html"
+  },
+  {
+    "revision": "96ce5360ffa8dc22105a",
+    "url": "/TP-PVT-LTD/static/css/main.c35e5d1f.chunk.css"
+  },
+  {
+    "revision": "87d0d94886d6794e8aed",
+    "url": "/TP-PVT-LTD/static/js/2.698931f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TP-PVT-LTD/static/js/2.698931f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ce5360ffa8dc22105a",
+    "url": "/TP-PVT-LTD/static/js/main.eeeb4cee.chunk.js"
+  },
+  {
+    "revision": "114e7cf67d07fd563840",
+    "url": "/TP-PVT-LTD/static/js/runtime-main.27d99069.js"
+  }
+]);
